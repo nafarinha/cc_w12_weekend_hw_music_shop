5b7cf0ecab70788fe08faea316b4e1068351aac8
Nuno Farinha <nadfarinha@gmail.com>
2019-04-05 17:36:36 +0100

    Create trumpet class

--- src/main/java/instruments/Trumpet.java
+++ src/main/java/instruments/Trumpet.java
@@ -1,4 +1,15 @@
 package instruments;
 
-public class Trumpet {
+public class Trumpet extends Instrument {
+
+    private int numValves;
+
+    public Trumpet(String make, String model, String colour, InstrumentType type, int numValves) {
+        super(make, model, colour, type);
+        this.numValves = numValves;
+    }
+
+    public int getNumValves() {
+        return this.numValves;
+    }
 }
--- src/test/java/TrumpetTest.java
+++ src/test/java/TrumpetTest.java
@@ -1,2 +1,76 @@
+import instruments.InstrumentType;
+import instruments.Trumpet;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TrumpetTest {
+
+    Trumpet trumpet1;
+
+    @Before
+    public void before() {
+        trumpet1 = new Trumpet("Henri Selmer", "B-flat", "Yellow", InstrumentType.BRASS, 3);
+    }
+
+    @Test
+    public void canGetNumValves() {
+        assertEquals(3, trumpet1.getNumValves());
+    }
+
+    @Test
+    public void canGetMake() {
+        assertEquals("Henri Selmer", trumpet1.getMake());
+    }
+
+    @Test
+    public void canGetModel() {
+        assertEquals("B-flat", trumpet1.getModel());
+    }
+
+    @Test
+    public void canGetColour() {
+        assertEquals("Yellow", trumpet1.getColour());
+    }
+
+    @Test
+    public void canGetPurchasePrice() {
+        assertEquals(0, trumpet1.getPurchasePrice(), 0);
+    }
+
+    @Test
+    public void canSetPurchasePrice() {
+        trumpet1.setPurchasePrice(5000);
+        assertEquals(5000, trumpet1.getPurchasePrice(),0);
+    }
+
+    @Test
+    public void canGetSellPrice() {
+        assertEquals(0, trumpet1.getSalePrice(), 0);
+    }
+
+    @Test
+    public void canSetSalePrice() {
+        trumpet1.setSalePrice(15000);
+        assertEquals(15000, trumpet1.getSalePrice(),0);
+    }
+
+    @Test
+    public void canGetType() {
+        assertEquals(InstrumentType.BRASS, trumpet1.getType());
+    }
+
+    @Test
+    public void canCalculateMarkup() {
+        trumpet1.setPurchasePrice(5000);
+        trumpet1.setSalePrice(15000);
+
+        assertEquals(2, trumpet1.calculateMarkup(), 0);
+    }
+
+    @Test
+    public void canPlaySound() {
+        assertEquals("Tarantara", trumpet1.play("Tarantara"));
+    }
 }
